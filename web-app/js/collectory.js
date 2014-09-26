3c0981ba9cd99cbcb576b27129be50909013bc8e
Mahmoud Sadeghi <mahmoud.sadeghi@csiro.au>
2014-09-26 10:25:48 +1000

    Usage stats are now an ordered list of breakdown by reasons in the following arrangement: - This Month - Last 3 Months - Last 12 months -All downloads
    Also, the testing stats are shown in grey.

--- web-app/js/collectory.js
+++ web-app/js/collectory.js
@@ -26,6 +26,21 @@ function capitaliseFirstLetter(string) {
 }
 
 /************************************************************\
+ * Sort a key-value array
+ \************************************************************/
+function sortKV(items) {
+    var values = [];
+    for (var item in items) {
+        values.push({ key: item, value: items[item] });
+    }
+    values.sort(function (a, b) {
+        return a.key.localeCompare(b.key);
+    });
+
+    return values;
+}
+
+/************************************************************\
 * Add commas to number strings
 \************************************************************/
 function addCommas(nStr){
@@ -176,10 +191,10 @@ function loadDownloadStats(loggerServicesUrl, uid, name, eventType) {
     }
 
     var displayNameMap = {
-        'all' : 'All downloads',
         'thisMonth' : 'This month',
         'last3Months' : 'Last 3 months',
-        'lastYear' : 'Last 12 months'
+        'lastYear' : 'Last 12 months',
+        'all' : 'All downloads'
     };
 
     $('div#usage').html("Loading statistics...");
@@ -194,12 +209,14 @@ function loadDownloadStats(loggerServicesUrl, uid, name, eventType) {
         },
         success: function (data) {
             $('div#usage').html('');
-            $.each(data, function( key, value ) {
+            $.each(displayNameMap, function( nameKey, displayString ) {
+                var value = data[nameKey];
                 var $usageDiv = $('<div class="usageDiv well"/>');
-                $usageDiv.html('<h4><span>' + displayNameMap[key] + "</span><span class='pull-right'>" + addCommas(value.records) + " records downloaded from " +  addCommas(value.events) + " downloads. </span></h4>");
+                $usageDiv.html('<h4><span>' + displayString + "</span><span class='pull-right'>" + addCommas(value.records) + " records downloaded from " +  addCommas(value.events) + " downloads. </span></h4>");
                 var $usageTable = $('<table class="table"/>');
-                $.each(data[key]['reasonBreakdown'], function( reason, details ) {
-                    var usageTableRow = $('<tr><td>' + capitalise(reason) + '</td><td>' + addCommas(details.events) + ' events</td><td>'  + addCommas(details.records)  + ' records </td>');
+                reasons = sortKV(value['reasonBreakdown']);
+                $.each(reasons, function( index, details ) {
+                    var usageTableRow = $('<tr'+((details.key.indexOf("test") >=0)?" style=color:#999999;":"")+'><td>' + capitalise(details.key) + '</td><td>' + addCommas(details.value.events) + ' events</td><td>'  + addCommas(details.value.records)  + ' records </td>');
                     $usageTable.append(usageTableRow);
                 });
                 $usageDiv.append($usageTable);
